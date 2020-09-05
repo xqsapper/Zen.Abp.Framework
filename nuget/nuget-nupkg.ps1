e3da1f5518cb34e8980dab85bdb348bf27928c0e
zen-arch <win8x@qq.com>
2020-09-05 15:04:39 +0800

    新增nuget编译提交

---
+++ nuget/nuget-nupkg.ps1
@@ -0,0 +1,35 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$slnPath = Join-Path $packFolder "../"
+$srcPath = Join-Path $slnPath "src"
+
+# List of projects
+$projects = (
+	"Zen.Abp.Domain.Shared",
+	"Zen.Abp.Application.Contracts",
+	"Zen.Abp.HttpApi.Hosting"
+)
+
+# Rebuild solution
+Set-Location $slnPath
+& dotnet restore
+Remove-Item -Recurse (Join-Path $packFolder "*.nupkg")
+
+# Copy all nuget packages to the pack folder
+foreach($project in $projects) {
+    
+    $projectFolder = Join-Path $srcPath $project
+
+    # Create nuget pack
+    Set-Location $projectFolder
+    Remove-Item -Recurse (Join-Path $projectFolder "bin/Release")
+    & dotnet msbuild /p:Configuration=Release /p:SourceLinkCreate=true
+    & dotnet msbuild /t:pack /p:Configuration=Release /p:SourceLinkCreate=true
+    # Copy nuget package
+    $projectPackPath = Join-Path $projectFolder ("/bin/Release/" + $project + ".*.nupkg")
+    Move-Item $projectPackPath $packFolder
+
+}
+
+# Go back to the pack folder
+Set-Location $packFolder
---
+++ nuget/nuget-push.bat
@@ -0,0 +1,3 @@
+cd ./
+nuget.exe push *.nupkg -apikey oy2oimnjgoj2v7f3xouakns4fhaw37quboaprl6r2ugndm -source https://api.nuget.org/v3/index.json
+pause
